130206b58582d698ada838baa7a23f6eabbb2049
Matt McCormick <matt.mccormick@kitware.com>
2012-01-10 13:15:49 -0500

    Write vtkOneSheetedHyperboloidSource test.
    Not building.

---
+++ test/vtkOneSheetedHyperboloidTest.cxx
@@ -0,0 +1,39 @@
+#include "vtkActor.h"
+#include "vtkPolyDataMapper.h"
+#include "vtkProperty.h"
+#include "vtkSmartPointer.h"
+#include "vtkOneSheetedHyperboloidSource.h"
+#include "vtkRenderWindow.h"
+#include "vtkRenderer.h"
+#include "vtkRenderWindowInteractor.h"
+#include <iostream>
+
+int main( int argc, char * argv[] )
+{
+  vtkSmartPointer< vtkOneSheetedHyperboloidSource > hyperboloid = vtkSmartPointer< vtkOneSheetedHyperboloidSource >::New();
+  //hyperboloid->SetThetaResolution( 50 );
+  //hyperboloid->SetZResolution( 50 );
+
+  vtkSmartPointer< vtkPolyDataMapper > mapper = vtkSmartPointer< vtkPolyDataMapper >::New();
+  mapper->SetInputConnection( hyperboloid->GetOutputPort() );
+
+  vtkSmartPointer< vtkActor > actor = vtkSmartPointer< vtkActor >::New();
+  actor->SetMapper( mapper );
+  vtkProperty * property = actor->GetProperty();
+  property->SetColor( 0.1, 0.7, 0.9 );
+
+  vtkSmartPointer< vtkRenderer > renderer = vtkSmartPointer< vtkRenderer >::New();
+  renderer->AddActor( actor );
+  renderer->SetBackground( 0.1, 0.1, 0.1 );
+
+  vtkSmartPointer< vtkRenderWindow > renderWindow = vtkSmartPointer< vtkRenderWindow >::New();
+  renderWindow->AddRenderer( renderer );
+  renderWindow->SetSize( 600, 600 );
+
+  vtkSmartPointer< vtkRenderWindowInteractor > renderWindowInteractor = vtkSmartPointer< vtkRenderWindowInteractor >::New();
+  renderWindowInteractor->SetRenderWindow( renderWindow );
+
+  renderWindowInteractor->Start();
+  
+  return 0;
+}
