547e57099f8b01a3225de4233e9d5f07d1b70cd2
Matt McCormick <matt.mccormick@kitware.com>
2012-01-12 17:35:30 -0500

    Display uni-axial tension.

--- test/vtkUniAxialLoadingTest.cxx
+++ test/vtkUniAxialLoadingTest.cxx
@@ -1,10 +1,12 @@
 #include "vtkActor.h"
+#include "vtkArrowSource.h"
 #include "vtkAxesActor.h"
 #include "vtkCubeSource.h"
 #include "vtkOneSheetedHyperboloidSource.h"
 #include "vtkOutlineFilter.h"
 #include "vtkPolyDataMapper.h"
 #include "vtkPolyDataNormals.h"
+#include "vtkProperty.h"
 #include "vtkSignedEigenvalueTensorGlyph.h"
 #include "vtkSmartPointer.h"
 #include "vtkSphereSource.h"
@@ -12,6 +14,7 @@
 #include "vtkStructuredPointsReader.h"
 #include "vtkTensorGlyph.h"
 #include "vtkTwoSheetedHyperboloidSource.h"
+#include "vtkTransform.h"
 #include "vtkRenderWindow.h"
 #include "vtkRenderer.h"
 #include "vtkRenderWindowInteractor.h"
@@ -77,6 +80,16 @@ int main( int argc, char * argv[] )
     return 1;
     }
 
+  vtkSmartPointer< vtkStructuredPointsReader > reader = vtkSmartPointer< vtkStructuredPointsReader >::New();
+  reader->SetFileName( argv[1] );
+  reader->Update();
+  vtkSmartPointer< vtkStructuredPoints > structuredPoints = reader->GetOutput();
+  structuredPoints->ComputeBounds();
+  double blockBounds[6];
+  structuredPoints->GetBounds( blockBounds );
+
+  std::cout << "block bounds: " << blockBounds[0] << " " << blockBounds[1] << std::endl;
+
   vtkSmartPointer< vtkPolyData > signedGlyphs;
   createGlyphs( argv[1], signedGlyphs );
 
@@ -93,13 +106,82 @@ int main( int argc, char * argv[] )
   vtkSmartPointer<vtkActor> outlineActor = vtkSmartPointer<vtkActor>::New();
   outlineActor->SetMapper( outlineMapper );
 
+  // the axes
   vtkSmartPointer<vtkAxesActor> axes = vtkSmartPointer<vtkAxesActor>::New();
+
+  // the plates
+  vtkSmartPointer<vtkCubeSource> bottomPlate = vtkSmartPointer<vtkCubeSource>::New();
+  double xLength = (blockBounds[1] - blockBounds[0]) * 1.2;
+  const double yLength = (blockBounds[3] - blockBounds[2]) / 10.0;
+  double zLength = (blockBounds[5] - blockBounds[4]) * 1.2;
+  bottomPlate->SetXLength( xLength );
+  bottomPlate->SetYLength( yLength );
+  bottomPlate->SetZLength( zLength );
+  bottomPlate->SetCenter( (blockBounds[1] - blockBounds[0]) / 2.0,
+    -yLength,
+    (blockBounds[5] - blockBounds[4])/ 2.0 );
+  vtkSmartPointer<vtkPolyDataMapper> bottomPlateMapper = vtkSmartPointer<vtkPolyDataMapper>::New();
+  bottomPlateMapper->SetInputConnection( bottomPlate->GetOutputPort() );
+  vtkSmartPointer<vtkActor> bottomPlateActor = vtkSmartPointer<vtkActor>::New();
+  bottomPlateActor->SetMapper( bottomPlateMapper );
+
+  vtkSmartPointer<vtkCubeSource> topPlate = vtkSmartPointer<vtkCubeSource>::New();
+  topPlate->SetXLength( xLength );
+  topPlate->SetYLength( yLength );
+  topPlate->SetZLength( zLength );
+  topPlate->SetCenter( (blockBounds[1] - blockBounds[0]) / 2.0,
+    blockBounds[3] + yLength,
+    (blockBounds[5] - blockBounds[4])/ 2.0 );
+  vtkSmartPointer<vtkPolyDataMapper> topPlateMapper = vtkSmartPointer<vtkPolyDataMapper>::New();
+  topPlateMapper->SetInputConnection( topPlate->GetOutputPort() );
+  vtkSmartPointer<vtkActor> topPlateActor = vtkSmartPointer<vtkActor>::New();
+  topPlateActor->SetMapper( topPlateMapper );
+
+  // the arrows
+  double arrowColor[3];
+  arrowColor[0] = 0.1;
+  arrowColor[1] = 0.7;
+  arrowColor[2] = 0.9;
+  double arrowCenter[3];
+
+  vtkSmartPointer<vtkArrowSource> topArrow = vtkSmartPointer<vtkArrowSource>::New();
+  vtkSmartPointer<vtkPolyDataMapper> topArrowMapper = vtkSmartPointer<vtkPolyDataMapper>::New();
+  topArrowMapper->SetInputConnection( topArrow->GetOutputPort() );
+  vtkSmartPointer<vtkActor> topArrowActor = vtkSmartPointer<vtkActor>::New();
+  topArrowActor->GetProperty()->SetColor( arrowColor );
+  topArrowActor->SetMapper( topArrowMapper );
+  vtkSmartPointer<vtkTransform> topArrowTransform = vtkSmartPointer<vtkTransform>::New();
+  arrowCenter[0] = (blockBounds[1] - blockBounds[0]) / 2.0;
+  arrowCenter[1] = blockBounds[3] + yLength * 3.0/2.0;
+  arrowCenter[2] = (blockBounds[5] - blockBounds[4]) / 2.0;
+  topArrowTransform->Translate( arrowCenter );
+  topArrowTransform->RotateZ( 90.0 );
+  topArrowActor->SetUserMatrix( topArrowTransform->GetMatrix() );
+
+  vtkSmartPointer<vtkArrowSource> bottomArrow = vtkSmartPointer<vtkArrowSource>::New();
+  bottomArrow->InvertOn();
+  vtkSmartPointer<vtkPolyDataMapper> bottomArrowMapper = vtkSmartPointer<vtkPolyDataMapper>::New();
+  bottomArrowMapper->SetInputConnection( bottomArrow->GetOutputPort() );
+  vtkSmartPointer<vtkActor> bottomArrowActor = vtkSmartPointer<vtkActor>::New();
+  bottomArrowActor->GetProperty()->SetColor( arrowColor );
+  bottomArrowActor->SetMapper( bottomArrowMapper );
+  vtkSmartPointer<vtkTransform> bottomArrowTransform = vtkSmartPointer<vtkTransform>::New();
+  arrowCenter[0] = (blockBounds[1] - blockBounds[0]) / 2.0;
+  arrowCenter[1] = blockBounds[2] - yLength * 3.0/2.0 - 1.0;
+  arrowCenter[2] = (blockBounds[5] - blockBounds[4]) / 2.0;
+  bottomArrowTransform->Translate( arrowCenter );
+  bottomArrowTransform->RotateZ( 90.0 );
+  bottomArrowActor->SetUserMatrix( bottomArrowTransform->GetMatrix() );
 
   vtkSmartPointer<vtkRenderer> renderer = vtkSmartPointer<vtkRenderer>::New();
   renderer->SetBackground( 0.2, 0.2, 0.2 );
   renderer->AddActor( actor );
   renderer->AddActor( outlineActor );
   renderer->AddActor( axes );
+  renderer->AddActor( bottomPlateActor );
+  renderer->AddActor( topPlateActor );
+  renderer->AddActor( topArrowActor );
+  renderer->AddActor( bottomArrowActor );
 
   vtkSmartPointer< vtkRenderWindow > renderWindow = vtkSmartPointer< vtkRenderWindow >::New();
   renderWindow->AddRenderer( renderer );
